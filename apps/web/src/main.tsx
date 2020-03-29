edd843fd43a2adc3f1c9cffd552980dbf816deb4
Wenbo Jie <pinkyjie.gn@gmail.com>
2020-03-29 15:15:52 +1100

    use emotion for front end styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './app/app';
+import { App } from './app/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
