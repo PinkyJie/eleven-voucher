ff5e31ea4573adf8ccb413729c428b76ae958a05
Wenbo Jie <pinkyjie.gn@gmail.com>
2020-04-01 20:34:11 +1100

    update firebase project

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   backendUrl:
-    'https://us-central1-eleven-locker.cloudfunctions.net/api/graphql',
+    'https://us-central1-eleven-voucher.cloudfunctions.net/api/graphql',
 };
