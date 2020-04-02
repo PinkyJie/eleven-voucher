439b3ec27cda388219a5ff4a099b4b189f273bac
Wenbo Jie <pinkyjie.gn@gmail.com>
2020-04-02 21:48:57 +1100

    add timestamp to all db records

--- apps/api/src/app/db/db.service.ts
+++ apps/api/src/app/db/db.service.ts
@@ -18,11 +18,15 @@ export class DbService {
     this.db = admin.firestore();
   }
 
+  private getServerTimestamp() {
+    return admin.firestore.FieldValue.serverTimestamp();
+  }
+
   async addNewUser(user: DbUser) {
     const userRef = this.db.collection('users');
     logger.log('Add new user to DB:');
     logger.log(user);
-    await userRef.add(user);
+    await userRef.add({ ...user, timestamp: this.getServerTimestamp() });
   }
 
   async getLatestFuelPriceRecord(fuelType: FuelType) {
@@ -38,13 +42,16 @@ export class DbService {
     const fuelPriceRef = this.db.collection('fuel_prices');
     logger.log('Add new fuel price to DB:');
     logger.log(fuelPrice);
-    await fuelPriceRef.add(fuelPrice);
+    await fuelPriceRef.add({
+      ...fuelPrice,
+      timestamp: this.getServerTimestamp(),
+    });
   }
 
   async addNewVoucher(voucher: DbVoucher) {
     const voucherRef = this.db.collection('vouchers');
     logger.log('Add new voucher to DB:');
     logger.log(voucher);
-    await voucherRef.add(voucher);
+    await voucherRef.add({ ...voucher, timestamp: this.getServerTimestamp() });
   }
 }
